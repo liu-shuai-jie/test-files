16772065af7075f8f132d8643d4e3b65aa887fd3
liuShuaiJie <342035299@qq.com>
2023-03-02 15:41:53 +0800

    1

--- src/views/test4.vue
+++ src/views/test4.vue
@@ -5,37 +5,42 @@
 </template>
 <script>
 export default {
-  name: 'test4',
-}
+  name: "test4",
+};
 </script>
 <script setup>
-import { reactive } from 'vue'
+import { reactive } from "vue";
 
 const arr1 = reactive([
-  { name: 'Facebook', age: 24 },
-  { name: 'Github', age: 18 },
-  { name: 'OpenAI', age: 18 },
-])
+  { name: "Facebook", age: 24 },
+  { name: "Github", age: 18 },
+  { name: "OpenAI", age: 18 },
+]);
 const arr2 = reactive([
-  { name: 'Google', age: 24 },
-  { name: 'Copilot', age: 20 },
-  { name: 'John', age: 24 },
-])
+  { name: "Google", age: 24 },
+  { name: "Copilot", age: 20 },
+  { name: "John", age: 24 },
+]);
 
-const groupByAge = function (arr1, arr2) {
-  let result = {}
-  let arr = arr1.concat(arr2) // 将数组中的元素去重
-  let temp = new Set(arr)
-  arr = [...temp] // 根据年龄分组
+const groupByAge = function (arr1 = [], arr2 = []) {
+  let result = {};
+  const arr = arr1.concat(arr2);
+  console.log(arr);
   arr.forEach((item) => {
-    let ageGroup = item.age
-    if (result[ageGroup]) {
-      result[ageGroup].push(item)
+    let age = item.age;
+    if (result[age]) {
+      // push之前判断里面有没有 避免重复
+      let flag = result[age].find(
+        (per) => per.name === item.name && per.age === item.age
+      );
+      if (!flag) {
+        result[age].push(item);
+      }
     } else {
-      result[ageGroup] = [item]
+      result[age] = [item];
     }
-  })
-  return result
-}
-console.log(groupByAge(arr1, arr2))
+  });
+  return result;
+};
+console.log(groupByAge(arr1, arr2));
 </script>
